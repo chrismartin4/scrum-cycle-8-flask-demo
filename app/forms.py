7903abc15dfd604156d1208d28aa7314d6c16e8a
Christopher Martin <chris.b411@gmail.com>
2022-06-27 18:13:16 -0500

    added forms and templates

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField, FileField, SelectField
-from wtforms.validators import InputRequired, Email, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField, PasswordField, FileField, SelectField, DateTimeField
+from wtforms.validators import InputRequired, Email, DataRequired
+from flask_wtf.file import FileRequired, FileAllowed
 
 class RegisterForm(FlaskForm):
     full_name = StringField('Full Name', validators=[InputRequired()])
     email = StringField('Email', validators=[Email()])
     password = PasswordField('Password', validators=[InputRequired()])
-    profile_photo=FileField('Upload Photo',validators=[FileRequired(),FileAllowed(['jpg','png', 'jpeg'],'Images only!')])
-    role=SelectField(u'Role' ,choices=[('Admin','Admin'),('Regular','Regular')])
+    profile_photo = FileField('Upload Photo', validators=[FileRequired(), FileAllowed(['jpg','png', 'jpeg'],'Images only!')])
+    role = SelectField(u'Role' ,choices=[('Admin','Admin'),('Regular','Regular')])
 
 class EventForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired()])
-    
+    start_date = DateTimeField('Start Date and Time', validators=[InputRequired()])
+    end_date = DateTimeField('End Date and Time', validators=[InputRequired()])
+    desc = TextAreaField('Description', validators=[InputRequired(),DataRequired()])
+    venue = StringField('Venue', validators=[InputRequired()])
+    flyer = FileField('Upload Photo', validators=[FileRequired(), FileAllowed(['jpg','png', 'jpeg'],'Images only!')])
+    website_url = StringField('Website URL', validators=[InputRequired()])
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, flash
 from app import app, db
-from .forms import ContactForm
+from .forms import RegisterForm, LoginForm, EventForm
 from .models import User
 import datetime
 
@@ -17,9 +17,9 @@ def about():
 def profile(username=None):
     return render_template('profile.html', username=username)
 
-@app.route('/contact', methods=["GET", "POST"])
-def contact():
-    form = ContactForm()
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
 
     if form.validate_on_submit():
         full_name = form.full_name.data # or request.form['full_name']
@@ -37,7 +37,51 @@ def contact():
         app.logger.error(error)
         flash(error)
 
-    return render_template('contact_form.html', form=form)
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        full_name = form.full_name.data # or request.form['full_name']
+        email = form.email.data # or request.form['email']
+        message = form.message.data # or request.form['message']
+
+        #app.logger.debug(full_name)
+        dt=datetime.datetime.now()
+        acc=User(full_name=full_name,email=email,password=message,date=dt)
+        if acc is not None:
+            db.session.add(acc)
+            db.session.commit()
+
+    for error in form.email.errors:
+        app.logger.error(error)
+        flash(error)
+
+    return render_template('login.html', form=form)
+
+@app.route('/addevent', methods=["GET", "POST"])
+def addevent():
+    form = EventForm()
+
+    if form.validate_on_submit():
+        full_name = form.full_name.data # or request.form['full_name']
+        email = form.email.data # or request.form['email']
+        message = form.message.data # or request.form['message']
+
+        #app.logger.debug(full_name)
+        dt=datetime.datetime.now()
+        acc=User(full_name=full_name,email=email,password=message,date=dt)
+        if acc is not None:
+            db.session.add(acc)
+            db.session.commit()
+
+    for error in form.errors:
+        app.logger.error(error)
+        flash(error)
+
+    return render_template('addevent.html', form=form)
 
 @app.route('/api/tasks')
 def tasks():
