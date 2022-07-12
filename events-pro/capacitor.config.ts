333fc92fd9f70ff948d9440a32ba529f0cef1ff4
Samantha876 <chinsamanthaashley@gmail.com>
2022-07-11 20:45:19 -0500

    ionic app created

---
+++ events-pro/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Events Pro',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
